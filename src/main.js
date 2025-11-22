480ed2e6d514e1f9ef08d1dfed6185c5fd8efece
Henry <drozdesh@gmail.com>
2025-11-22 22:20:45 +0500

    test_4

--- src/main.js
+++ src/main.js
@@ -48,7 +48,10 @@ function analyzeSalesData(data, options) {
     !data ||
     !Array.isArray(data.sellers) ||
     !Array.isArray(data.products) ||
-    !Array.isArray(data.purchase_records)
+    !Array.isArray(data.purchase_records) ||
+    data.sellers.length === 0 ||
+    data.products.length === 0 ||
+    data.purchase_records.length === 0
   ) {
     throw new Error("Некорректные входные данные");
   }
