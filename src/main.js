c3ad031ff0c0b68f79cd5ddfaf66c3724d4e4533
Henry <drozdesh@gmail.com>
2025-11-22 22:51:23 +0500

    test_7

--- src/main.js
+++ src/main.js
@@ -56,14 +56,17 @@ function analyzeSalesData(data, options) {
     throw new Error("Некорректные входные данные");
   }
   
-  const sellerStats = data.sellers.map((seller) => ({
+  const sellerStats = data.sellers.map((seller) => ([
+    seller.id,
+    {
     id: seller.id,
     name: `${seller.first_name} ${seller.last_name}`,
     revenue: 0,
     profit: 0,
     sales_count: 0,
     products_sold: {},
-  }));
+  }
+]));
 
   const sellerIndex = Object.fromEntries(
     sellerStats.map((seller) => [seller.id, seller])
